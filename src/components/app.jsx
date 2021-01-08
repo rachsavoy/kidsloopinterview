556277d77a39e2233a1392312ada26d4c68fc2b7
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-08 13:31:04 +0900

    delete unused code.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,11 +5,6 @@ import "./Login.css";
 class App extends Component {
   render() {
     return(
-      <div className="card">
-        <img src="https://drive.google.com/file/d/1qsvlH1AkFNiCCEXvU7jC-eJ5CFHj1HJr/view?usp=sharing" alt="KidsLoop logo" />
-        <h4> Sign In</h4>
-        <SigninForm>
-      </div>
     );
   }
 }
--- src/components/signin_form.jsx
+++ src/components/signin_form.jsx
@@ -1,46 +1,11 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 class SigninForm extends Component {
   render(){
-  function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+  return ();
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email or Phone*</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password*</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
-    </div>
-  );
-}
 
-export default SigninForm;
+export default ;
